a21f1e1bba4273a92e59c26a4f014ec5ce41143b
kushal <collabskus@gmail.com>
2026-03-11 16:40:27 -0400

    refactor person to its own file

--- HelloNugetPackageReference.ClassLibrary/NewtonsoftJsonSampler.cs
+++ HelloNugetPackageReference.ClassLibrary/NewtonsoftJsonSampler.cs
@@ -3,19 +3,6 @@ using Newtonsoft.Json.Linq;
 
 namespace HelloNugetPackageReference.ClassLibrary;
 
-public class Person
-{
-    public string Name { get; set; } = string.Empty;
-    public int Age { get; set; }
-    public string[] Hobbies { get; set; } = [];
-
-    [JsonIgnore]
-    public string InternalNote { get; set; } = "hidden";
-
-    [JsonProperty("contact_email")]
-    public string Email { get; set; } = string.Empty;
-}
-
 public static class NewtonsoftJsonSampler
 {
     public static void Run()
---
+++ HelloNugetPackageReference.ClassLibrary/Person.cs
@@ -0,0 +1,16 @@
+﻿using Newtonsoft.Json;
+
+namespace HelloNugetPackageReference.ClassLibrary;
+
+public class Person
+{
+    public string Name { get; set; } = string.Empty;
+    public int Age { get; set; }
+    public string[] Hobbies { get; set; } = [];
+
+    [JsonIgnore]
+    public string InternalNote { get; set; } = "hidden";
+
+    [JsonProperty("contact_email")]
+    public string Email { get; set; } = string.Empty;
+}
