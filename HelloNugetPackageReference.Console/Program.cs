5d3941218b5817073ff6367d96fc06190dc02e74
kushal <collabskus@gmail.com>
2026-03-11 17:35:58 -0400

    verify I can access newtonsoft from console

--- HelloNugetPackageReference.Console/Program.cs
+++ HelloNugetPackageReference.Console/Program.cs
@@ -1,3 +1,33 @@
 ﻿using HelloNugetPackageReference.ClassLibrary;
+using Newtonsoft.Json;
 
 NewtonsoftJsonSampler.Run();
+
+Person person = new Person
+{
+    Name = "Alice",
+    Age = 30,
+    Hobbies = ["cycling", "reading"],
+    InternalNote = "should not appear",
+    Email = "alice@example.com"
+};
+
+string json = JsonConvert.SerializeObject(person, Formatting.Indented);
+Console.WriteLine("=== Serialized ===");
+Console.WriteLine(json);
+
+// 2. Deserialize JSON back to an object
+string incoming = """
+            {
+                "Name": "Bob",
+                "Age": 25,
+                "Hobbies": ["gaming", "cooking"],
+                "contact_email": "bob@example.com"
+            }
+            """;
+
+Person? newPerson = JsonConvert.DeserializeObject<Person>(incoming);
+if (null !=  newPerson)
+{
+    Console.WriteLine(newPerson.Name);
+}
